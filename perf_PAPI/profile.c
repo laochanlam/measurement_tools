92015566acf3e2e0a5931e54d00fd7b133ba2c46
laochanlam <laochanlam@gmail.com>
2019-04-24 19:15:10 +0800

    TLB test

--- perf_PAPI/profile.c
+++ perf_PAPI/profile.c
@@ -1,8 +1,8 @@
 #include "profile.h"
 
-int EVENTSIZE = 8;
+#define EVENTSIZE  4
 int EventSet;
-long_long start[8], end[8], values[8];
+long_long start[EVENTSIZE], end[EVENTSIZE], values[EVENTSIZE];
 
 void init_and_start_collect() {
     if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT)
@@ -12,19 +12,42 @@ void init_and_start_collect() {
     if (PAPI_create_eventset(&EventSet) != PAPI_OK)
         exit(-1);
 
-    // [0]Total cycles
-    if (PAPI_add_event(EventSet, PAPI_TOT_CYC) != PAPI_OK)
+// Section1 CPI 
+//    if (PAPI_add_event(EventSet, PAPI_TOT_CYC) != PAPI_OK)
+//        exit(-1);
+//   if (PAPI_add_event(EventSet, PAPI_LST_INS) != PAPI_OK)
+//        exit(-1);
+
+
+// Section2 L1 cache miss rate
+//    if (PAPI_add_event(EventSet, PAPI_SR_INS) != PAPI_OK)
+//        exit(-1);
+//    if (PAPI_add_event(EventSet, PAPI_LD_INS) != PAPI_OK)
+//        exit(-1);
+//    if (PAPI_add_event(EventSet, PAPI_L1_TCM) != PAPI_OK)
+//        exit(-1);
+	
+ 
+// Section3 L2 cache miss rate
+//    if (PAPI_add_event(EventSet, PAPI_L1_TCM) != PAPI_OK)
+//        exit(-1);
+//    if (PAPI_add_event(EventSet, PAPI_L2_TCM) != PAPI_OK)
+//        exit(-1);
+
+
+// Section4 TLB miss rate
+    if (PAPI_add_event(EventSet, PAPI_LD_INS) != PAPI_OK)
         exit(-1);
-    // [1]Load/store instructions completed
-    if (PAPI_add_event(EventSet, PAPI_LST_INS) != PAPI_OK)
+    if (PAPI_add_event(EventSet, PAPI_SR_INS) != PAPI_OK)
         exit(-1);
-    // [2]Level 1 data cache misses
-    if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
+    if (PAPI_add_event(EventSet, PAPI_TLB_DM) != PAPI_OK)
         exit(-1);
-    // [3]instruction cache misses
-    if (PAPI_add_event(EventSet, PAPI_L1_ICM) != PAPI_OK)
-        exit(-1);
-    // [4]Level 1 cache misses
+//    if (PAPI_add_event(EventSet, PAPI_TLB_IM) != PAPI_OK)
+//        exit(-1);
+
+
+
+/*   // [4]Level 1 cache misses
     if (PAPI_add_event(EventSet, PAPI_L1_TCM) != PAPI_OK)
         exit(-1);
     // [5]Level 1 load misses
@@ -34,6 +57,7 @@ void init_and_start_collect() {
     if (PAPI_add_event(EventSet, PAPI_L1_STM) != PAPI_OK)
         exit(-1);
     printf("add okall");
+*/
     if (PAPI_start(EventSet) != PAPI_OK)
         exit(-1);
 
@@ -57,12 +81,21 @@ void end_of_collect(){
 
 void calculate_and_printout(){
 
-    // for 
-    // printf("PAPI_TOT_CYC:%lld\nPAPI_LST_INS: %lld\n",values[0], values[1]);
-    // printf("CPI: %f\n", values[1]/(float)values[0]);
 
         for (int i = 0; i < EVENTSIZE; i++) {
             values[i] = end[i] - start[i];
-            printf("%lld\n", values[i]);
+//            printf("%lld\n", values[i]);
         }
+
+// Section1 CPI
+//        printf("CPI: %f\n", (values[1]/(float)values[0]));
+
+// Section2 L1 cache miss rate
+//        printf("L1 cache miss rate: %f\n", values[2]/(float)(values[0]+values[1]));
+
+// Section3 L2 cache miss rate
+//        printf("L2 cache miss rate: %f\n", values[1]/(float)(values[0]));
+
+// Section4 TLB miss rate
+        printf("TLB miss rate: %f\n", (values[2]+values[3])/(float)(values[0]+values[1]));
 }
