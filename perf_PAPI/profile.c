73df051f83748e84fa0c8e28037c81a009c42303
laochanlam <laochanlam@gmail.com>
2019-04-24 16:28:21 +0800

    L1 cache bug test

--- perf_PAPI/profile.c
+++ perf_PAPI/profile.c
@@ -1,12 +1,8 @@
 #include "profile.h"
 
-    int EventSet;
-
-    int i, sum;
-
-    long_long start[2], end[2], values[2];
-
-
+int EVENTSIZE = 8;
+int EventSet;
+long_long start[8], end[8], values[8];
 
 void init_and_start_collect() {
     if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT)
@@ -16,16 +12,28 @@ void init_and_start_collect() {
     if (PAPI_create_eventset(&EventSet) != PAPI_OK)
         exit(-1);
 
+    // [0]Total cycles
     if (PAPI_add_event(EventSet, PAPI_TOT_CYC) != PAPI_OK)
         exit(-1);
-
+    // [1]Load/store instructions completed
     if (PAPI_add_event(EventSet, PAPI_LST_INS) != PAPI_OK)
         exit(-1);
-
-    //if (PAPI_add_event(EventSet, ) != PAPI_OK)
-    //    exit(-1);
-
-
+    // [2]Level 1 data cache misses
+    if (PAPI_add_event(EventSet, PAPI_L1_DCM) != PAPI_OK)
+        exit(-1);
+    // [3]instruction cache misses
+    if (PAPI_add_event(EventSet, PAPI_L1_ICM) != PAPI_OK)
+        exit(-1);
+    // [4]Level 1 cache misses
+    if (PAPI_add_event(EventSet, PAPI_L1_TCM) != PAPI_OK)
+        exit(-1);
+    // [5]Level 1 load misses
+    if (PAPI_add_event(EventSet, PAPI_L1_LDM) != PAPI_OK)
+        exit(-1);
+    // [6]Level 1 store misses
+    if (PAPI_add_event(EventSet, PAPI_L1_STM) != PAPI_OK)
+        exit(-1);
+    printf("add okall");
     if (PAPI_start(EventSet) != PAPI_OK)
         exit(-1);
 
@@ -33,9 +41,6 @@ void init_and_start_collect() {
         exit(-1);
 }
 
-//
-  //  for (i=0;i<10000;i++)
-   // sum+=i;
 void end_of_collect(){
     if (PAPI_stop(EventSet, end) != PAPI_OK)
         exit(-1);
@@ -51,13 +56,13 @@ void end_of_collect(){
     /* Get value */
 
 void calculate_and_printout(){
-    values[0]=end[0]-start[0];
 
-    values[1]=end[1]-start[1];
+    // for 
+    // printf("PAPI_TOT_CYC:%lld\nPAPI_LST_INS: %lld\n",values[0], values[1]);
+    // printf("CPI: %f\n", values[1]/(float)values[0]);
 
-    
-    printf("PAPI_TOT_CYC:%lld\nPAPI_LST_INS: %lld\n",values[0], values[1]);
-    printf("CPI: %f\n", values[1]/(float)values[0]);
-
-
+        for (int i = 0; i < EVENTSIZE; i++) {
+            values[i] = end[i] - start[i];
+            printf("%lld\n", values[i]);
+        }
 }
--- perf_PAPI/profile.h
+++ perf_PAPI/profile.h
@@ -3,11 +3,5 @@
 #include "papi.h"
 
 void init_and_start_collect();
-
-//
-  //  for (i=0;i<10000;i++)
-   // sum+=i;
 void end_of_collect();
-    /* Get value */
-
 void calculate_and_printout();
